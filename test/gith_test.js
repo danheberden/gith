b0d345dd735af265719e26f7cde2279db6caf35c
Dan Heberden <danheberden@gmail.com>
2012-09-12 23:50:29 -0700

    add server listener, eventEmitter on generated gith object and public close and payload functions

--- test/gith_test.js
+++ test/gith_test.js
@@ -1,4 +1,4 @@
-var Gith = require('../lib/gith.js');
+var githFactory = require('../lib/gith.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -25,25 +25,34 @@ exports['constructor'] = {
     // setup here
     done();
   },
-  'constructor (Gith) has create method': function(test) {
+  'constructor (githFactory) has create method': function(test) {
     test.expect(1);
-    test.ok( Gith.create, "require('gith') has .create method");
+    test.ok( githFactory.create, "require('gith') has .create method");
     test.done();
   },
-  'Gith.create is a function and can set port': function( test ) {
+  'githFactory.create is a function and can set port': function( test ) {
     test.expect( 3 );
-    test.strictEqual( typeof Gith.create, 'function', 'Gith.create is a function' );
-    var gith = Gith.create( 9001 );
-    test.strictEqual( gith.port, 9001, 'Gith.create assigned a port correctly' );
-    var gith2 = Gith.create();
-    test.strictEqual( gith2.port, undefined, 'Gith.create works fine with no port' );
+    test.strictEqual( typeof githFactory.create, 'function', 'githFactory.create is a function' );
+    var gith = githFactory.create( 9001 );
+    test.strictEqual( gith.port, 9001, 'githFactory.create assigned a port correctly' );
+    var gith2 = githFactory.create();
+    test.strictEqual( gith2.port, undefined, 'githFactory.create works fine with no port' );
+    gith.close();
     test.done();
   },
   'Created giths are unique': function( test ) {
     test.expect( 1 );
-    var g1 = Gith.create(8000);
-    var g2 = Gith.create(8000);
+    var g1 = githFactory.create(8000);
+    var g2 = githFactory.create(8001);
     test.notEqual( g1, g2, 'Should be unique things, yo' );
+    g1.close();
+    g2.close();
+    test.done();
+  },
+  'function based way to create if someone wants to': function( test ) {
+    test.expect( 1 );
+    var gith = githFactory(1984);
+    test.ok( gith.listen );
     test.done();
   }
 };
@@ -54,12 +63,14 @@ exports['gith.listen'] = {
   },
   'listen assigns port': function( test ) {
     test.expect( 2 );
-    var gith = Gith.create();
+    var gith = githFactory.create();
     gith.listen( 9001 );
     test.strictEqual( gith.port, 9001, 'gith.listen should assign the port' );
-    var gith2 = Gith.create( 9000 );
-    gith2.listen( 9001 );
-    test.strictEqual( gith2.port, 9001, 'gith.listen overrites port' );
+    var gith2 = githFactory.create( 9000 );
+    gith2.listen( 9002 );
+    test.strictEqual( gith2.port, 9002, 'gith.listen overrites port' );
+    gith.close();
+    gith2.close();
     test.done();
   }
 };
@@ -68,11 +79,12 @@ exports['gith events'] = {
   setUp: function( done ) {
     done();
   },
-  'gith has .emit and .on': function( test ) {
+  'gith() has .emit and .on': function( test ) {
     test.expect( 2 );
-    var gith = Gith.create();
+    var gith = githFactory.create();
     test.ok( gith().on, '.on present' );
     test.ok( gith().emit, '.emit present' );
     test.done();
   }
 };
+
