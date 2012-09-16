d7a0b2c01cb6c65368313759e75c34040308c63f
Dan Heberden <danheberden@gmail.com>
2012-09-16 14:38:01 -0700

    add tests for filtering, events and all kinds of cool shit

--- test/gith_test.js
+++ test/gith_test.js
@@ -1,5 +1,6 @@
 var githFactory = require('../lib/gith.js');
 var http = require('http');
+var _ = require('lodash');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -52,7 +53,7 @@ exports['constructor'] = {
   },
   'function based way to create if someone wants to': function( test ) {
     test.expect( 1 );
-    var gith = githFactory(1984);
+    var gith = githFactory();
     test.ok( gith.listen );
     test.done();
   }
@@ -76,7 +77,7 @@ exports['gith.listen'] = {
   }
 };
 
-exports['gith events'] = {
+exports['gith eventemitter'] = {
   setUp: function( done ) {
     done();
   },
@@ -96,18 +97,204 @@ exports['gith server'] = {
   'gith creates a server and listens to payloads on that port': function( test ) {
     test.expect(1);
     var gith = githFactory.create( 9001 );
-    var payloadObject = { test: 'valid' };
-    gith().on( '*', function( data, payload ) {
-      test.equal( payload.test, payloadObject.test, "raw payload data should equal sent payload data" );
+    
+    var payloadObject = require('./payloads/add-file-and-dir.json');
+    var failSafe = false;
+    gith().on( 'all', function( payload ) {
+      failSafe = true;
+      test.equal( payload.original.after, payloadObject.after, "payload data should equal sent payload data" );
       gith.close();
       test.done();
     });
-    // create a server to send data 
-    var requestServer = http.createClient( 9001, 'localhost' );
-    var request = requestServer.request( 'POST', '/', { 'host': 'localhost' } );
+
+    // just incase
+    setTimeout( function() {
+      if ( !failSafe ) {
+        gith.close();
+        test.ok( false, 'payload event never fired after 200ms, shutting down server' );
+        test.done();
+      }
+    }, 200 );
+
+    var request = http.request({
+      port: 9001,
+      host: 'localhost',
+      method: 'POST'
+    });
     request.write( 'payload=' + JSON.stringify( payloadObject ) );
     request.end();
   }
 };
-  
 
+exports['gith filtering'] = { 
+  setUp: function( done ) {
+    done();
+  },
+  'string matching': function( test ) {
+    test.expect(3);
+    var json = require( './payloads/create-branch-with-files.json' );
+    var gith = githFactory.create();
+    
+    var test1 = gith({
+      repo: 'danheberden/payloads'
+    }).on( 'all', function( payload ) {
+      test.ok( true, 'repo text matched' );
+    });
+
+    var test2 = gith({
+      repo: 'danheberden/payloads',
+      file: 'README.md',
+      branch: 'merge-test'
+    }).on( 'all', function( payload ) {
+      test.ok( true, 'repo and file and branch text matched' );
+    });
+
+    var test3 = gith({
+      branch: 'merge-test'
+    }).on( 'all', function( payload ) {
+       test.ok( true, 'just branch should match' );
+    });
+
+    var test4 = gith({
+      repo: 'wrong/repo'
+    }).on( 'all', function( payload ) {
+      test.ok( false, 'wrong/repo should have never matched' );
+   //   test.done();
+    });
+
+    var test5 = gith({
+      repo: 'danheberden/payloads',
+      branch: 'nup'
+    }).on( 'all', function( payload ) {
+      test.ok( false, 'repo should have matched, but not branch, so.....' );
+    });
+
+    // this needs to be improved
+    setTimeout( function(){
+      test.done();
+    }, 500 );
+
+    gith.payload( json );
+  },
+  'regex matching': function( test ) {
+    test.expect(3);
+    var json = require( './payloads/tag-v1.0.0.json' );
+    var gith = githFactory.create();
+    
+    var test1 = gith({
+      repo: /payloads/
+    }).on( 'all', function( payload ) {
+      test.ok( true, 'repo regexp should match' );
+    });
+
+    var test2 = gith({
+      repo: 'danheberden/payloads',
+      tag: /^v1\.(.*)/
+    }).on( 'tag:add', function( payload ) {
+      test.ok( true, 'tag regex should match' );
+      test.equal( payload.matches.tag[1], '0.0', 'regex matches should be stored' );
+    });
+
+    var test3 = gith({
+      tag: /yeahright/
+    }).on( 'all', function( payload ) {
+       test.ok( false, 'non-matching regex should fail' );
+    });
+
+    // this needs to be improved
+    setTimeout( function(){
+      test.done();
+    }, 500 );
+
+    gith.payload( json );
+  }
+};
+
+ 
+exports['gith events'] = {
+  setUp: function( done ) {
+    done();
+  },
+  'add-file-and-dir.json': function( test ) {
+    test.expect(1);
+    var json = require( './payloads/add-file-and-dir.json' );
+    var gith = githFactory.create();
+    var g = gith();
+
+    g.on( 'file:add', function( payload ) {
+      test.deepEqual( ['delete-me.txt', 'payloads/add-file.json' ], payload.files.all,
+                       'matched both files' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'delete-file.json': function( test ) {
+    var json = require( './payloads/delete-file.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'file:delete', function( payload ) {
+      test.deepEqual( [ 'another2.txt' ], payload.files.deleted, 
+                      'delete branch event should have deleted branch' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'add-file.json': function( test ) {
+    var json = require( './payloads/add-file.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'file:add', function( payload ) {
+      test.deepEqual( [ 'README.md' ], payload.files.all,
+                      'matched both files' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'create-branch-with-files.json': function( test ) {
+    var json = require( './payloads/create-branch-with-files.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'branch:add', function( payload ) {
+      test.equal( payload.branch, 'merge-test', 'branch add should match payload' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'tag-v1.0.0.json': function( test ) {
+    var json = require( './payloads/tag-v1.0.0.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'tag:add', function( payload ) {
+      test.equal( payload.tag, 'v1.0.0', 'tag add event should fire the right tag, yo' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'delete-remote-tag.json': function( test ) {
+    var json = require( './payloads/delete-remote-tag.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'tag:delete', function( payload ) {
+      test.equal( payload.tag, 'v1.0.0', 'tag delete event should fire the right tag' );
+      test.done();
+    });
+    gith.payload( json );
+  },
+  'delete-remote-branch.json': function( test ) {
+    var json = require( './payloads/delete-remote-branch.json' );
+    test.expect(1);
+    var gith = githFactory.create();
+    var g = gith();
+    g.on( 'branch:delete', function( payload ) {
+      test.equal( payload.branch, 'merge-test', 'delete branch event should have deleted branch' );
+      test.done();
+    });
+    gith.payload( json );
+  }
+
+};
